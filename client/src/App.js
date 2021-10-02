bf52243ce589de8e26e1eb46ceb108115c78cd37
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-03 03:24:21 +0500

    Implementing Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 /** @format */
+// imports
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// Pages
+import Home from "./booking/Home";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
 
 function App() {
-  return <div>hello World</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/auth/Login.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+const Login = () => {
+  re  return <div className='container-fluid h1 p-5 text-center'>Login Page</div>;
+};
+
+export default Login;
