4b8fa60f686901924804f7cab8f1832e04bb2384
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-03 04:11:10 +0500

    Code Refactor

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+/** @format */
+
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "LOGGED_IN_USER":
+      return { ...state, ...action.payload };
+    case "LOGOUT":
+      return action.payload;
+    default:
+      return state;
+  }
+};
