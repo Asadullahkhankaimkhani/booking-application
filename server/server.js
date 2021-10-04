892f737bf535b2fadbe53c6a3b4cb8b9b7b87387
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-04 22:18:55 +0500

    Express Server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
 /** @format */
 
-console.log("Server test is running");
+const express = require("express");
+
+const app = express();
+
+app.get("/api/:message", (req, res) => {
+  res.status(200).send(req.params.message);
+});
+
+app.listen(8000, () => {
+  console.log(`Server is running on Port ${8000}`);
+});
