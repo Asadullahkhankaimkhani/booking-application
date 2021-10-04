3fa1177ce7b593ff390c1d3c887062a60e9fb64b
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-04 22:40:14 +0500

    Controller MVC model

---
+++ server/controllers/auth.js
@@ -0,0 +1,5 @@
+/** @format */
+
+export const showMessage = (req, res) => {
+  res.status(200).send(req.params.message);
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,11 @@
 
 import express from "express";
 
+//controller
+import { showMessage } from "../controllers/auth";
+
 const router = express.Router();
 
-router.get("/:message", (req, res) => {
-  res.status(200).send(req.params.message);
-});
+router.get("/:message", showMessage);
 
 module.exports = router;
