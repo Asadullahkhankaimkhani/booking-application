6db9080694c50c2bc9da41e7cf54176b19b5ffdd
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-07 18:52:04 +0500

    Register Form

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,10 +1,70 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ name, email, password });
+  };
+
+  const showForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          className="form-control"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-group mb-3">
+        <label htmlFor="name" className="form-label">
+          Email
+        </label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          className="form-control"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group mb-3">
+        <label htmlFor="password" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          className="form-control"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="btn btn-primary mt-2">Register</button>
+    </form>
+  );
+
   return (
-    <div className='container-fluid h1 p-5 text-center'>Register Page</div>
+    <>
+      <div className="container-fluid h1 p-5 bg-secondary text-center">
+        <h1>Register Page</h1>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">{showForm()}</div>
+        </div>
+      </div>
+    </>
   );
 };
 
