1a78dc99e912d789d6d31c399b9a8d489277bed0
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-07 21:19:42 +0500

    Save User Database

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,26 @@
 /** @format */
+import User from "../model/User";
 
-export const showMessage = (req, res) => {
-  res.status(200).send(req.params.message);
-};
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
 
-export const register = (req, res) => {
-  console.log(req.body);
+  if (!name) return res.status(400).send("Name is Required");
+  if (!password) return res.status(400).send("Password is Required");
+  if (password.length < 6)
+    return res.status(400).send("Password Should be Greater than 6 Character");
+
+  const existedUser = await User.findOne({ email }).exec();
+
+  if (existedUser) return res.status(400).send("Email Already Taken");
+
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    console.log("User is Registered");
+    res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Error Try Again");
+  }
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,12 +3,9 @@
 import express from "express";
 
 //controller
-import { showMessage, register } from "../controllers/auth";
+import { register } from "../controllers/auth";
 
 const router = express.Router();
-
-// test router
-router.get("/:message", showMessage);
 
 // User Routes
 router.post("/register", register);
