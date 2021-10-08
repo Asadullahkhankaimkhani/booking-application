1e01e15d3d5e85fb3c3d2ecbae917277b5515b0f
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-08 20:13:25 +0500

    Redux State on Page Load

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -4,22 +4,30 @@ import React, { useState } from "react";
 import LoginForm from "../components/LoginForm";
 import { login } from "../actions/auth";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
 
-const Login = () => {
+const Login = ({ history }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Data send to backend", { email, password });
 
     //
     try {
       const { data } = await login({ email, password });
       if (data) {
         // Save token in local storage and redux state
+        window.localStorage.setItem("auth", JSON.stringify(data));
+
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: data,
+        });
       }
-      console.log(data);
+      history.push("/");
     } catch (err) {
       console.log(err);
       if (err.response.status === 400) toast.error(err.response.data);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,14 @@
 /** @format */
 
-export const authReducer = (state = {}, action) => {
+let userState;
+
+if (window.localStorage.getItem("auth")) {
+  userState = JSON.parse(window.localStorage.getItem("auth"));
+} else {
+  userState = null;
+}
+
+export const authReducer = (state = userState, action) => {
   switch (action.type) {
     case "LOGGED_IN_USER":
       return { ...state, ...action.payload };
