232618fc2b3b60440706f43a108c09c612e92611
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-13 03:26:56 +0500

    Conditional Rendering

--- client/src/user/SellerDashboard.js
+++ client/src/user/SellerDashboard.js
@@ -2,8 +2,36 @@ import React from "react";
 import DashboardNav from "../components/DashboardNav";
 import ConnectNav from "../components/ConnectNav";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const SellerDashboard = () => {
+  const { auth } = useSelector((state) => ({ ...state }));
+
+  const connected = () => (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-10">
+          <h2>Your Hotels</h2>
+        </div>
+        <div className="col-md-2">
+          <Link to="/hotels/new">
+            <button className="btn btn-primary">+ Add Hotel</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+
+  const notConnected = () => (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-10">
+          <h2>Connect With Stripe</h2>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <div className="container-fluid bg-secondary p-5 text-center">
@@ -12,18 +40,13 @@ const SellerDashboard = () => {
       <div className="container-fluid p-4">
         <DashboardNav />
       </div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-10">
-            <h2>Your Hotels</h2>
-          </div>
-          <div className="col-md-2">
-            <Link to="/hotels/new">
-              <button className="btn btn-primary">+ Add Hotel</button>
-            </Link>
-          </div>
-        </div>
-      </div>
+      {auth &&
+      auth.user &&
+      auth.user.token &&
+      auth.user.stripe_seller &&
+      auth.user.stripe_seller.charges_enabled
+        ? connected()
+        : notConnected()}
     </>
   );
 };
