bccd8c8413455762ce2210341c19898162bc4c6a
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-21 21:48:55 +0500

    Get account Status in Stripe callback

--- client/src/stripe/StripeCallback.js
+++ client/src/stripe/StripeCallback.js
@@ -1,7 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { LoadingOutlined } from "@ant-design/icons";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getAccountStatus } from "../actions/stripe";
 
 const StripeCallback = ({ history }) => {
+  const { auth } = useSelector((state) => ({ ...state }));
+  //const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (auth && auth.token) {
+      accountStatus();
+    }
+    // eslint-disable-next-line no-use-before-define
+  }, [auth]);
+
+  const accountStatus = async () => {
+    const { data } = await getAccountStatus(auth.token);
+    console.log("Account DATA", data);
+  };
+
   return (
     <div className="d-flex justify-content-center p-5">
       <LoadingOutlined className="display-1 p-5 text-danger" />
