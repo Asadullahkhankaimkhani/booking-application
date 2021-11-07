b3e0c53ca0869a34ae714b26573681a8f8cdc767
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-11-07 12:32:47 +0500

    Debug Headers already send error

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,20 +3,21 @@ import User from "../model/User";
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  if (!name) return res.status(400).send("Name is Required");
-  if (!password) return res.status(400).send("Password is Required");
-  if (password.length < 6)
-    return res.status(400).send("Password Should be Greater than 6 Character");
-
-  const existedUser = await User.findOne({ email }).exec();
-
-  if (existedUser) return res.status(400).send("Email Already Taken");
-
-  const user = new User(req.body);
-
   try {
+    const { name, email, password } = req.body;
+
+    if (!name) return res.status(400).send("Name is Required");
+    if (!password) return res.status(400).send("Password is Required");
+    if (password.length < 6)
+      return res
+        .status(400)
+        .send("Password Should be Greater than 6 Character");
+
+    const existedUser = await User.findOne({ email }).exec();
+
+    if (existedUser) return res.status(400).send("Email Already Taken");
+
+    const user = new User(req.body);
     await user.save();
     console.log("User is Registered");
     res.json({ ok: true });
